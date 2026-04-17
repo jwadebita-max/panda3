d439930155dbb77a4c29d2ee106cc1d9f4c3975f
jwadebita-max <jwadebita@gmail.com>
2026-04-17 15:19:57 +0000

    this is new

--- game.py
+++ game.py
@@ -3,6 +3,10 @@ from direct.showbase.showbase import ShowBase
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        
+        self.model = loader.loadModel('models/environme')
+        self.model.reparentTo(render)
+        self.model.setScale(0.25,0.25,0.25)
+        self.model.setPos(-8,42,0)
+        base.camera.setPos(0,-20,3)
 base = ShowBase()
 base.run()
